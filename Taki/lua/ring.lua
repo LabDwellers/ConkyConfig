e4d8bed4c360b05be3f4a566f0c482d32b2fbb54
Sataki <troy19_91@hotmail.com>
2012-03-20 15:09:22 -0700

    Made clock rings more '3D'

--- Taki/lua/ring.lua
+++ Taki/lua/ring.lua
@@ -20,27 +20,43 @@ function conky_clock()
 	local seconds = tonumber(os.date("%S"))
 	seconds = seconds / 60.0
 	conky_ring(8, 120, 1.0, 1.0, 1.0, 0.2, 0, 1)
-	conky_ring(8, 120, 0.3, 0.86, 1.0, 1.0, 0, seconds)
+	conky_ring(1, 116.5, 0.15, 0.43, 0.5, 1.0, 0, seconds)
+	conky_ring(1, 117.5, 0.225, 0.645, 0.75, 1.0, 0, seconds)
+	conky_ring(4, 120, 0.3, 0.86, 1.0, 1.0, 0, seconds)
+	conky_ring(1, 122.5, 0.225, 0.645, 0.75, 1.0, 0, seconds)
+	conky_ring(1, 123.5, 0.15, 0.43, 0.5, 1.0, 0, seconds)
 
 	local minutes = tonumber(os.date("%M"))
 	minutes = minutes / 60.0
 	conky_ring(12, 108, 0.44, 0.44, 0.44, 0.2, 0, 1)
-	conky_ring(12, 108, 0.0, 0.37, 0.44, 1.0, 0, minutes)
+	conky_ring(1, 102.5, 0.0, 0.185, 0.22, 1.0, 0, minutes)
+	conky_ring(2, 104, 0.0, 0.2775, 0.33, 1.0, 0, minutes)
+	conky_ring(6, 108, 0.0, 0.37, 0.44, 1.0, 0, minutes)
+	conky_ring(2, 112, 0.0, 0.2775, 0.33, 1.0, 0, minutes)
+	conky_ring(1, 113.5, 0.0, 0.185, 0.22, 1.0, 0, minutes)
 
 	local hours = tonumber(os.date("%H"))
 	hours = hours / 24.0
 	conky_ring(6, 97, 0.73, 0.73, 0.73, 0.2, 0, 1)
-	conky_ring(6, 97, 0.0, 0.73, 0.60, 1.0, 0, hours)
+	conky_ring(1, 94.5, 0.0, 0.365, 0.30, 1.0, 0, hours)
+	conky_ring(4, 97, 0.0, 0.73, 0.60, 1.0, 0, hours)
+	conky_ring(1, 99.5, 0.0, 0.365, 0.30, 1.0, 0, hours)
 
 	local day = tonumber(os.date("%d"))
 	day = day / 31.0
 	conky_ring(4, 90, 0.46, 0.46, 0.46, 0.2, 0, 1)
-	conky_ring(4, 90, 0.0, 0.46, 0.38, 1.0, 0, day)
+	conky_ring(1, 88.5, 0.0, 0.23, 0.19, 1.0, 0, day)
+	conky_ring(2, 90, 0.0, 0.46, 0.38, 1.0, 0, day)
+	conky_ring(1, 91.5, 0.0, 0.23, 0.19, 1.0, 0, day)
 
 	local month = tonumber(os.date("%m"))
 	month = month / 12.0
 	conky_ring(10, 81, 0.82, 0.82, 0.82, 0.2, 0, 1)
-	conky_ring(10, 81, 0.23, 0.63, 0.82, 1.0, 0, month)
+	conky_ring(1, 76.5, 0.115, 0.315, 0.41, 1.0, 0, month)
+	conky_ring(1, 77.5, 0.1725, 0.4725, 0.615, 1.0, 0, month)
+	conky_ring(6, 81, 0.23, 0.63, 0.82, 1.0, 0, month)
+	conky_ring(1, 84.5, 0.1725, 0.4725, 0.615, 1.0, 0, month)
+	conky_ring(1, 85.5, 0.115, 0.315, 0.41, 1.0, 0, month)
 	--Clock ring seperators
 	--conky_ring(2, 115, 0.0, 0.0, 0.0, 1.0, 0, 1)
 	--conky_ring(2, 101, 0.0, 0.0, 0.0, 1.0, 0, 1)
