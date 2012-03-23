28d4954c0b0b9cd4743701ad3e4ec4c685b7c29d
Zanterian <zanterian@gmail.com>
2012-03-22 23:12:58 -0700

    Adding network info

--- Zanty/dev/lua/rings1.lua
+++ Zanty/dev/lua/rings1.lua
@@ -18,6 +18,20 @@ function conky_ring(width, radius, colorR, colorG, colorB, colorA, start, perc)
 	cairo_stroke(cr)
 end
 
+function reverse_conky_ring(width, radius, colorR, colorG, colorB, colorA, start, perc)
+	if conky_window == nil then return end
+	if cs == nil or cairo_xlib_surface_get_width(cs) ~= conky_window.width or cairo_xlib_surface_get_height(cs) ~= conky_window.height then
+		if cs then cairo_surface_destroy(cs)end
+		cs = cairo_xlib_surface_create(conky_window.display, conky_window.drawable, conky_window.visual, conky_window.width, conky_window.height)
+	end
+	if cr then cairo_destroy(cr) end
+	cr = cairo_create(cs)
+	cairo_set_line_width (cr, width)
+	cairo_set_source_rgba (cr, colorR, colorG, colorB, colorA)
+	cairo_arc_negative (cr, radius+(width/2), radius+(width/2), radius, start, start-perc*2*math.pi)
+	cairo_stroke(cr)
+end
+
 -- For filling
 function conky_circle(width, radius, colorR, colorG, colorB, colorA, start, perc, height)
 	if conky_window == nil then return end
@@ -88,6 +102,43 @@ function conky_core4(x)
 	
 	conky_circle(3, ((tonumber(conky_parse("${cpu cpu4}")))/100)*(((tonumber(x)-3)/2) - 3), 0.56, 0.01, 0.99, 1.0, starting_point, 1, x)
 	conky_circle(3, ((tonumber(x)-3)/2) - 3, 0.43, 0.43, 0.43, 0.2, starting_point, 1, x)
+end
+
+function conky_network(x)
+	local starting_point = 3*math.pi / 2
+	local upload_speed = tonumber(conky_parse("${upspeedf wlan0}"))
+	local download_speed = tonumber(conky_parse("${downspeedf wlan0}"))
+	-- Time for percents
+	local upload_percent = 0
+	local download_percent = 0
+	-- Color values
+	-- upload
+	local u_r = 0.015
+	local u_g = 0.999
+	local u_b = 0.592
+	-- download 0.96, 0.01, 0.23
+	local d_r = 0.96
+	local d_g = 0.01
+	local d_b = 0.23
+	-- Upload Speed conditions
+	if upload_speed < 100 then
+		upload_percent = upload_speed / 100
+		else
+		upload_percent = 1
+	end
+	
+	if download_speed < 500 then
+		download_percent = download_speed / 500
+		else
+		download_percent = 1
+	end
+	
+	conky_circle(10, ((tonumber(conky_parse("${wireless_link_qual_perc wlan0}")))/100)*(((tonumber(x)-10)/2) - 10), 0.56, 0.01, 0.99, 1.0, starting_point, 1, x)
+	conky_circle(10, 1*(((tonumber(x)-10)/2) - 10), 0.43, 0.43, 0.43, 0.2, starting_point, 1, x)
+	
+	conky_ring(5, (tonumber(x)-5)/2, 0.43, 0.43, 0.43, 0.2, starting_point, 1)
+	conky_ring(5, (tonumber(x)-5)/2, u_r, u_g, u_b, 1, starting_point, upload_percent/2)
+	reverse_conky_ring(5, (tonumber(x)-5)/2, d_r, d_g, d_b, 1, starting_point, download_percent/2)
 end
 
 -- Cleanup
